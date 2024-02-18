ef7553d56c432fcb567786f5d60c00bc653d27d6
Sadia <148858244+Openclassroomstudent@users.noreply.github.com>
2024-02-18 09:37:43 +0100

    js

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,29 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+document.addEventListener("DOMContentLoaded", function() {
+	const arrowLeft = document.getElementById('arrow_left');
+	const arrowRight = document.getElementById('arrow_right');
+	const dotsContainer = document.querySelector('.dots');
+	let activeIndex = 0;
+  
+	arrowLeft.addEventListener('click', slideLeft);
+	arrowRight.addEventListener('click', slideRight);
+
+// Création des dots
+for (let i = 0; i < slides.length; i++) {
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	if (i === activeIndex) {
+	  dot.classList.add('dot_selected');
+	}
+	dotsContainer.appendChild(dot);
+  }
+
+  const dots = document.querySelectorAll('.dot');
+  for (let i = 0; i < dots.length; i++) {
+	dots[i].addEventListener('click', () => {
+	  changeSlide(i);
+	});
+  }
+});
